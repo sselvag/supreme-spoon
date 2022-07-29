e8a448993799f55409eb8474fa208e5e8dc65655
sselvag <swaathy.selva@gmail.com>
2022-07-28 23:28:03 -0400

    used switch instead

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import {ApolloClient,InMemoryCache,ApolloProvider,createHttpLink,} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
